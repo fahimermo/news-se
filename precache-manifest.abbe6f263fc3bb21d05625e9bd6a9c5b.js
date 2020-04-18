15c1ec6842bbf0105fd2ac10b653df6eb46c6c06
elegant-moon <elegantmoon.tech@gmail.com>
2020-04-18 15:06:43 +0200

    Updates

---
+++ precache-manifest.abbe6f263fc3bb21d05625e9bd6a9c5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a73b5ff35d418f082440a920d4ea514",
+    "url": "/news-se/index.html"
+  },
+  {
+    "revision": "a0cf7fbb114fdf9d108b",
+    "url": "/news-se/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "1f27f36370dcedd9630e",
+    "url": "/news-se/static/js/2.69ce6252.chunk.js"
+  },
+  {
+    "revision": "a0cf7fbb114fdf9d108b",
+    "url": "/news-se/static/js/main.1eaf8bcb.chunk.js"
+  },
+  {
+    "revision": "6f5a6d26bfeb0454e03b",
+    "url": "/news-se/static/js/runtime~main.9a8c4326.js"
+  }
+]);
